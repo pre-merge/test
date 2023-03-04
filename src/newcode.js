472db9ed2020f516594e399212ee9c5da8700738
Aziz Kurt <kurt@pureple.com>
2023-03-04 11:35:29 -0800

    Update

--- src/newcode.js
+++ src/newcode.js
@@ -9,6 +9,6 @@ export function calculate(num1, num2, operator) {
     case "/":
       return num1 / num2;
     default:
-      return "Invalid operator";
+      return "Invalid operator 1";
   }
 }
